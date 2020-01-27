33e8fa20a8bb6d2e081698e4bd26dc8c2f42377e
Jarffery <jerrylee0128@gmail.com>
2020-01-27 12:19:39 -0800

    updates

--- my script/useful_script/test.py
+++ my script/useful_script/test.py
@@ -12,11 +12,27 @@ extract the most features from the syllabi using regular expressions.
 5. save a csv file called features-retrieved-by-JohnSmith.csv (all lower case) that has 100 rows,  each containing the features extracted.  Replace the JohnSmith with your name.
 '''
 import  PyPDF2
+import pandas
+import os
 
-pdfFileobj = open('.\syllabi/13205.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileobj)
-print(ldfReader.numPages)
-pageObj = pdfReader.getPage(0)
-print(pageObj.extractText())
+g = os.walk(r"C:/Users/Novogene/Documents/GitHub/Python-Note/my script/useful_script/syllabi")
+for path, dir_list, file_list in g:
+    for file_name in file_list:
+        #print(os.path.join(path, file_name))
+        print(file_name)
+        
+class PDF_TO_CSV(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.feature_dict = {}
+    def Open_pdf(self,filename):
+        
+    pdfFileobj = open('.\syllabi/13205.pdf', 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileobj)
+    pdfReader.numPages
+    for pages in range(pdfReader.numPages):
+        print(pages)
+        pageObj = pdfReader.getPage(pages)
+        print(pageObj.extractText())
 
 
